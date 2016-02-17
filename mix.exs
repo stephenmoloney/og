6d617677baa2f005ee9cba37a4044b2a3212c1a8
Stephen Moloney <stephenmoloney@live.com>
2016-02-17 23:36:00 +0000

    version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Og.Mixfile do
     [
      app: :og,
      name: "Óg",
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
