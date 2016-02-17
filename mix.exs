7f19bd6e1e3a9417113608146897e66f381323b5
Stephen Moloney <stephenmoloney@live.com>
2016-02-17 23:25:55 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Og.Mixfile do
     [
      app: :og,
      name: "Og",
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
